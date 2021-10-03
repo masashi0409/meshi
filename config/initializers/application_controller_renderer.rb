8b29a1ae06f49dc65f48c3deb57080b6aeca1cee
ShimanoMasashi <masashi.shimano0409@gmail.com>
2021-10-03 05:43:10 +0000

    meshiterro作成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'b06c884d886667d770c4abdf5f720d1c2c4cb830b107593fe9dc1df3b46d1e185b1a4f697740cb2e9e4eb641bf8473f8c7f3578d7f99068c6f17dcd6101c2f46'
